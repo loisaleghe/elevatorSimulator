5b7de94600f8b7112837e121ebb91047dbd02540
Abdul-Rahmaan Rufai <abdul-rahmaanrufai@Abdul-Rahmaans-MacBook-Air.local>
2020-01-31 18:42:46 -0500

    pull

--- src/elevatorSimulator/FloorSubsystem.java
+++ src/elevatorSimulator/FloorSubsystem.java
@@ -1,5 +1,40 @@
 package elevatorSimulator;
 
-public class FloorSubsystem {
+import java.util.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
+public class FloorSubSystem implements Runnable {
+	
+	private Floor floor;
+	private Elevator elevator;
+	private boolean up;
+	private boolean down;
+	
+	public void FloorSubsystem(Floor floor, Elevator elevator) {
+		this.floor = floor;
+		this.elevator = elevator;
+	}
+	
+	public void getTimeStamp() {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println(timestamp);
+	}
+	
+	public void run() {
+		
+		int floorNumber = floor.getFloorNumber(floor);
+		String direction = elevator.getDirection();
+		int car = elevator.getCarNumber();
+		
+		while(elevator.ElevatorArrived()) {
+			
+		}
+		
+		while(true) {
+			if(up == true) {
+				
+			}
+		}
+	}
 }
