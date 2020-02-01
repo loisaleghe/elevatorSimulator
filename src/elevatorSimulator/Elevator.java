98d3aa470cd8ae950d1e7795dc331b55ad7d8470
ediomo emah <ediomoemah@dhcp-149-143.cu-wireless-students.carleton.ca>
2020-01-31 20:06:10 -0500

    finished the Elevator class

--- src/elevatorSimulator/Elevator.java
+++ src/elevatorSimulator/Elevator.java
@@ -23,7 +23,10 @@ public class Elevator {
 		 this.currFloor = new Floor(1);
 	}
 	 
-	
+	/*
+	 * add floors to either the elevator going up or the elevator
+	 * going down
+	 */
 	public void addFloor (FloorData f) {
 		
 		//gets the destination of the floor that is pressed 
@@ -46,6 +49,9 @@ public class Elevator {
 		
 	}
 	
+	/*
+	 * dictates the movement of the elevator
+	 */
 	public void move () {
 		
 		//scenarios for the elevator moving up
@@ -56,12 +62,59 @@ public class Elevator {
 				return;
 			}
 			
+			//increments the floor if the current floor is less than the floor at index 0 
 			if (currFloor.getNumber() < upQueue.peek().getNumber()) {
 				currFloor.setNumber(currFloor.getNumber() + 1);
 			}
 			
+			//removes the floor from the queue if the current floor is the same
+			//as the floor at that specific index
+			else if (currFloor.getNumber() == upQueue.peek().getNumber()) {
+				upQueue.poll();
+				
+				//if the queue is empty, reverse the direction
+				if (upQueue.isEmpty()) {
+					currDirection = Direction.DOWN;
+					
+				}
+				
+				else{
+					//increments the current floor is the queue isn't empty
+					currFloor.setNumber(currFloor.getNumber() + 1);
+				}
+			}
 			
+		}
+		
+		//scenarios for the elevator moving down
+		else if (currDirection.equals(Direction.DOWN)) {
 			
+			//if the queue is empty, nothing should happen
+			if (downQueue.isEmpty()) {
+				return;
+			}
+			
+			//decrements the floor if the current floor is greater than the floor at index 0 
+			if (currFloor.getNumber() > downQueue.peek().getNumber()) {
+				currFloor.setNumber(currFloor.getNumber() - 1);
+			}
+			
+			//removes the floor from the queue if the current floor is the same
+			//as the floor at that specific index
+			else if (currFloor.getNumber() == downQueue.peek().getNumber()) {
+				downQueue.poll();
+				
+				//if the queue is empty, reverse the direction
+				if(downQueue.isEmpty()) {
+					currDirection = Direction.UP;
+				}
+				
+				else {
+					//decrements the current floor is the queue isn't empty
+					currFloor.setNumber(currFloor.getNumber() - 1);
+					
+				}	
+			}
 		}
 	}
 	
