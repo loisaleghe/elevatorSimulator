12f672e5e259a196e20ae8edf8c2c77c7c1f8ba0
ediomo emah <ediomoemah@ediomos-MacBook-Pro.local>
2020-02-13 23:31:15 -0500

    updated Elevator test class

--- src/elevatorSimulator/ElevatorTest.java
+++ src/elevatorSimulator/ElevatorTest.java
@@ -26,12 +26,11 @@ class ElevatorTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
-	/*@Test
-	void test() {
-		fail("Not yet implemented");
-	}*/
 	
+	/*
+	 * Checks floors are added correctly in the right 
+	 * queue
+	 */
 	@Test
 	public void testAddFloorsToUpQueue() {
 		elevator.setCurrentFloor(new Floor (2));
@@ -40,5 +39,43 @@ class ElevatorTest {
 		elevator.addFloor(new Floor(3));
 		assertTrue((elevator.getUpQueue().peek()).equals(new Floor(3)));	
 	}
+	
+	/*
+	 * Checks floors are added correctly in the right 
+	 * queue
+	 */
+	@Test
+	public void testAddFloorsToDownQueue() {
+		elevator.setCurrentFloor(new Floor (7));
+		elevator.addFloor(new Floor (2));
+		elevator.addFloor(new Floor (6));
+		elevator.addFloor(new Floor (4));
+		elevator.addFloor(new Floor (3));
+		assertTrue((elevator.getDownQueue().peek()).equals(new Floor(6)));
+	}
+	
+	/*
+	 * Prints the size of the two queues
+	 */
+	@Test
+	public void testSizeOfElevatorFloors() {
+		elevator.setCurrentFloor(new Floor (5));
+		elevator.addFloor(new Floor (2));
+		elevator.addFloor(new Floor (7));
+		elevator.addFloor(new Floor (9));
+		elevator.addFloor(new Floor (4));
+		System.out.println(elevator.getUpQueue().size());
+		System.out.println(elevator.getDownQueue().size());
+	}
+	
+	@Test
+	public void testMoveUp() {
+		elevator.setCurrentFloor(new Floor (2));
+		elevator.addFloor(new Floor (3));
+		elevator.setCurrentDirection(Direction.UP);
+		elevator.move();
+		System.out.println(elevator.getCurrentFloor());
+	}
+	
 
 }
