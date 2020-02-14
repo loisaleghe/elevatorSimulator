b14f5b578db8a9afc8b921bf604563e17749b0bb
hilairedjani <djanilewis1@gmail.com>
2020-02-13 21:29:14 -0500

    - Added equals and toString methods for Floor class

--- src/elevatorSimulator/Floor.java
+++ src/elevatorSimulator/Floor.java
@@ -30,5 +30,34 @@ public class Floor {
 	public void setNumber(int number) {
 		this.number = number;
 	}
+	
+	/**
+	 * Checks if this floor is equal to another given floor
+	 * @param o, an Object, represents the floor being compared to this floor
+	 * @return a boolean, true if this floor is the same as the given floor, false otherwise
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if(this == o)	
+			return true;
+		
+		if(o == null)
+			return false;
+		
+		if (this.getClass() != o.getClass())
+	        return false;
+		
+		Floor f = (Floor) o;
+		
+		return this.number == f.number;
+	}
+	
+	/**
+	 * Gives a string representation of this floor
+	 * @return a String, representing this floor
+	 */
+	public String toString() {
+		return "floor: " + this.number;
+	}
 
 }
