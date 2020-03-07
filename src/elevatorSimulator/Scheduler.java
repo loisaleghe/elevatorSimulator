dc62c68bc68c1d84ba5855f13309df1fc8a273fc
Abdul-Rahmaan Rufai <abdul-rahmaanrufai@Abdul-Rahmaans-MacBook-Air.local>
2020-03-06 19:30:44 -0500

    pulling/pushing

--- src/elevatorSimulator/Scheduler.java
+++ src/elevatorSimulator/Scheduler.java
@@ -1,5 +1,13 @@
 package elevatorSimulator;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 /*
@@ -15,9 +23,16 @@ public class Scheduler extends Thread {
 	private ArrayList<Elevator> elevators;	// List of elevators
 	private boolean canSendData;
 	private boolean canGetData;
+	
+	private DatagramSocket sendSocket;
+	private DatagramSocket fsReceive;
+	private DatagramSocket esReceive;
+	private DatagramPacket sendPacket;
+	private ArrayList<DatagramPacket> receivePacket;
 
 	//the scheduler stops when moreData is false
 	private boolean moreData;
+	
 
 	/*
 	 * The constructor to define the instance variables 
@@ -31,6 +46,16 @@ public class Scheduler extends Thread {
 		this.moreData = true;
 		this.floorData = new ArrayList<>();	
 		this.elevators = new ArrayList<>();
+		
+		try {
+			sendSocket = new DatagramSocket(30);
+			esReceive = new DatagramSocket(20);
+			fsReceive = new DatagramSocket(10);
+		}
+		catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 	public boolean getMoreData() {
@@ -47,17 +72,18 @@ public class Scheduler extends Thread {
 	
 	/**
 	 * Adds the specified elevator to the list of elevators handled by this scheduler
-	 * @param e an Elevator, represents the elevator to be added to the list of elevators handled by this schedular
+	 * @param e an Elevator, represents the elevator to be added to the list of elevators handled by this scheduler
 	 */
 	public void addElevator(Elevator e) {
 		this.elevators.add(e);
 	}
 
+		
 
 	/*
 	 * This method is called by the Floor and Elevator Subsystems
 	 * to continuously send data to the scheduler
-	 */
+	 
 	public synchronized void sendData(FloorData fl) {
 		//		Wait if flood data is being processed
 		while(!this.canSendData) {
@@ -68,6 +94,14 @@ public class Scheduler extends Thread {
 			}
 		}
 
+		try {
+			bytesToFloorData
+		}
+		catch(UnknownHostException e){
+			e.printStackTrace();
+	         System.exit(1);
+		}
+		
 		System.out.println("== Schedular: Received request on floor " + fl.getFloor());
 		//this.floorData = new FloorData(fl);
 		this.floorData.add(fl);				//Populating floor data arraylist with floordata then sending it to the elevator subsystem. Iteration 2
@@ -76,13 +110,14 @@ public class Scheduler extends Thread {
 
 		notifyAll();
 	}
+	*/
 
 	/*
 	 * This method is called by the Floor and Elevator Subsystems
 	 * to continuously get data from the scheduler
 	 * 
 	 * Modified to return an arraylist of floordata. Iteration 2
-	 */
+	 
 	public synchronized ArrayList<Floor> getData(Floor f) {
 		// Wait if there is no floor data available
 		ArrayList<Floor> temp = new ArrayList<>();
@@ -131,6 +166,36 @@ public class Scheduler extends Thread {
 
 		System.out.println("== Schedular: Sending floor requests to Elevator subsystem " + temp);
 		return temp;
+	}
+	*/
+	
+	
+	public void bytesToFloorData() {
+		
+		
+		try {
+			if(!receivePacket.isEmpty()) {
+				for(DatagramPacket dP : receivePacket) {
+					ByteArrayInputStream bais = new ByteArrayInputStream(dP.getData());
+					ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new BufferedInputStream(bais)));
+					
+					Object o = ois.readObject();
+					ois.close();
+					
+					if(o instanceof FloorData) {
+						FloorData fd = (FloorData) o;
+						System.out.println("Scheduler received floordata containing :" + fd.toString() + "\n");
+					}
+				}
+			}
+		}
+		
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
